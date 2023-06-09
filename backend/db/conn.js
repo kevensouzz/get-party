c21628a3b4658d21f680d5a9562a9f9a826e2f7a
Keven Souza <kevenssouza1@gmail.com>
2023-06-09 19:40:40 -0300

    config environment variables with dotenv

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,17 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function main() {
+  const dbHOST = process.env.DB_HOST;
+  const dbUSER = process.env.DB_USER;
+  const dbPASSWORD = process.env.DB_PASSWORD;
+
+  const dbURL = `mongodb+srv://${dbUSER}:${dbPASSWORD}@${dbHOST}/?retryWrites=true&w=majority`;
+
   try {
-    mongoose.set("strictQuery", true)
+    mongoose.set("strictQuery", true);
 
-    await mongoose.connect(
-      "mongodb+srv://kevensouzz:KEVENSS2006@api-rest.dkqy8oh.mongodb.net/?retryWrites=true&w=majority"
-    )
-
-    console.log("sucessfully connected to db!");
+    await mongoose
+      .connect(dbURL)
+      .then(() => {
+        console.log("sucessfully connected to db!");
+      });
   } catch (error) {
     console.log(error);
   }
 }
 
-module.exports = main
+module.exports = main;
