05bfa68634cd96b568e5d183bc612f3e4d8c65df
Keven Souza <kevenssouza1@gmail.com>
2023-06-16 17:04:16 -0300

    add nav's buttons to sm screens

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,34 +1,54 @@
+"use client";
+
+import { List, X } from "@phosphor-icons/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Nav() {
+  const [menu, setMenu] = useState(false);
+
+  const abrirFechar = () => {
+    setMenu(!menu);
+  };
   return (
     <nav
-      className={`flex items-center justify-between w-1/4 text-xl max-xl:w-1/3 max-lg:w-1/2 max-md:text-base max-sm:hidden`}
+      className={`flex items-center justify-between w-1/4 text-xl max-xl:w-1/3 max-lg:w-1/2 max-md:text-base max-sm:w-min`}
     >
-      <Link
-        href={"/"}
-        className={`transition-all duration-200 ease-linear hover:text-red-600`}
+      <span
+        className={`flex items-center justify-between w-full max-sm:hidden`}
       >
-        Home
-      </Link>
-      <Link
-        href={"/services"}
-        className={`transition-all duration-200 ease-linear hover:text-red-600`}
+        <Link
+          href={"/"}
+          className={`transition-all duration-200 ease-linear hover:text-red-600`}
+        >
+          Home
+        </Link>
+        <Link
+          href={"/services"}
+          className={`transition-all duration-200 ease-linear hover:text-red-600`}
+        >
+          Services
+        </Link>
+        <Link
+          href={"/about"}
+          className={`transition-all duration-200 ease-linear hover:text-red-600`}
+        >
+          About
+        </Link>
+        <Link
+          href={"/contact"}
+          className={`transition-all duration-200 ease-linear hover:text-red-600`}
+        >
+          Contact
+        </Link>
+      </span>
+
+      <span
+        className={`flex items-center justify-centerl sm:hidden cursor-pointer transition-all duration-200 ease-linear hover:text-red-600`}
+        onClick={abrirFechar}
       >
-        Services
-      </Link>
-      <Link
-        href={"/about"}
-        className={`transition-all duration-200 ease-linear hover:text-red-600`}
-      >
-        About
-      </Link>
-      <Link
-        href={"/contact"}
-        className={`transition-all duration-200 ease-linear hover:text-red-600`}
-      >
-        Contact
-      </Link>
+        {menu ? <X size={32} /> : <List size={32} />}
+      </span>
     </nav>
   );
 }
