05041f6221fb294ef8ee0cd3981294f13a437176
Keven Souza <kevenssouza1@gmail.com>
2023-06-17 21:04:43 -0300

    create Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className={`flex flex-col items-center justify-center w-full h-full`}>
-      Home
+    <main className={`flex flex-col justify-start items-center w-full h-full`}>
+      <section
+        className={`w-full h-1/2 flex flex-col justify-center items-center text-4xl max-lg:text-2xl max-md:text-lg max-sm:text-sm max-[375px]:px-14`}
+      >
+        Serviços de festa de altíssima qualidade!
+        <span>
+          <Link href={"/services"} className={`text-red-600 pr-2`}>
+            Clique aqui
+          </Link>
+          e confira agora nossos serviços!
+        </span>
+      </section>
+
+      <section
+        className={`w-full h-1/2 flex flex-col justify-start items-center text-4xl  max-lg:text-2xl max-md:text-lg max-sm:text-sm max-[375px]:px-14`}
+      >
+        Mais de 10 anos atuando no mercado!
+        <span>
+          <Link href={"/about"} className={`text-red-600 pr-2`}>
+            Clique aqui
+          </Link>
+          e saiba mais sobre nós!
+        </span>
+      </section>
     </main>
   );
 }
