84d7c44f73d904b9031b3f129731f654b28c3b61
Keven Souza <kevenssouza1@gmail.com>
2023-06-21 20:12:45 -0300

    update pages

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,7 +1,3 @@
 export default function About() {
-  return (
-    <main className={`flex flex-col items-center justify-center w-full h-full`}>
-      About
-    </main>
-  );
+  return <main className={`w-full h-full`}></main>;
 }
