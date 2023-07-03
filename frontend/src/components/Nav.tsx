fd1b2ecbdd4ffe69e58b2e60a448edaec9af7b11
Keven Souza <kevenssouza1@gmail.com>
2023-07-03 20:31:11 -0300

    diferent style on current path

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,15 +1,15 @@
 "use client";
-import Link from "next/link";
 import { useState } from "react";
 import NavModal from "./NavModal";
 import { AlignJustify } from "lucide-react";
+import NavLink from "./NavLink";
 
 export default function Nav() {
   const [menu, setMenu] = useState(false);
-
   const abrirFechar = () => {
     setMenu(!menu);
   };
+
   return (
     <nav
       className={`flex items-center justify-between h-full w-1/4 text-xl max-xl:w-1/3 max-lg:w-1/2 max-md:text-base max-sm:w-min`}
@@ -17,10 +17,10 @@ export default function Nav() {
       <span
         className={`flex items-center justify-between w-full max-sm:hidden`}
       >
-        <Link href={"/"}>Home</Link>
-        <Link href={"/services"}>Services</Link>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/contact"}>Contact</Link>
+        <NavLink children={"Home"} href={"/"} />
+        <NavLink children={"Services"} href={"/services"} />
+        <NavLink children={"About"} href={"/about"} />
+        <NavLink children={"Contact"} href={"/contact"} />
       </span>
 
       <span
