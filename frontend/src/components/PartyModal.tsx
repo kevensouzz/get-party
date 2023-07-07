2bff0d8d21567700866368f7538b1967ef686f42
Keven Souza <kevenssouza1@gmail.com>
2023-07-07 17:01:57 -0300

    update PartyModal style

--- frontend/src/components/PartyModal.tsx
+++ frontend/src/components/PartyModal.tsx
@@ -1,11 +1,24 @@
+import { X } from "lucide-react";
+
 export default function PartyModal({ onClose }: { onClose: () => void }) {
   return (
     <section
       className={`fixed inset-0 bg-black bg-opacity-75 backdrop-blur-md flex flex-col justify-center items-center`}
     >
-      <span onClick={onClose} className={`text-white cursor-pointer`}>
-        close modal
-      </span>
+      <main
+        className={`bg-black overflow-hidden select-text flex flex-col rounded-2xl border border-red-600 
+        w-[500px] h-[500px] max-sm:w-[375px] max-sm:h-[425px] max-[425px]:w-[250px] max-[425px]:h-[375px]`}
+      >
+        <header
+          className={`w-full h-1/6 bg-red-600 flex items-center justify-between px-4`}
+        >
+          <h1 className="font-semibold"></h1>
+          <X onClick={onClose} className={`cursor-pointer w-8 h-8`} />
+        </header>
+        <section
+          className={`w-full h-full flex flex-col items-center justify-between`}
+        ></section>
+      </main>
     </section>
   );
 }
