d13f020fdadadeab3c8b12e321dcd9b09f3b0395
Keven Souza <kevenssouza1@gmail.com>
2023-07-18 12:17:34 -0300

    update modal layout

--- backend/models/party.js
+++ backend/models/party.js
@@ -1,39 +1,37 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const partySchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
+const Party = mongoose.model(
+  "Party",
+  new Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      author: {
+        type: String,
+        required: true,
+      },
+      budget: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      services: {
+        type: [
+          {
+            name: String,
+            price: Number,
+          },
+        ],
+      },
     },
-    author: {
-      type: String,
-      required: true,
-    },
-    budget: {
-      type: Number,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    services: {
-      type: [
-        {
-          name: String,
-          price: Number,
-        },
-      ],
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
+  )
 );
 
-const Party = mongoose.model("Party", partySchema);
-
-module.exports = {
-  Party,
-  partySchema,
-};
+module.exports = Party;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,23 +1,25 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const User = mongoose.model(
-    'User',
-    new Schema({
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        }
+  "User",
+  new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    },
+    { timestamps: true }
+  )
+);
 
-    }, { timestamps: true }),
-)
-
-module.exports = User
+module.exports = User;
