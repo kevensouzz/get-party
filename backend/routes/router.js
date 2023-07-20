994fb4550bf2db145b8664f5dce7215c205567e4
Keven Souza <kevenssouza1@gmail.com>
2023-07-20 11:23:28 -0300

    add endpoint /

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,11 +1,16 @@
-const router = require("express").Router()
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  const links = { Parties: "/Parties", Users: "/users" };
+  res.json(links);
+});
 
 // parties
-const partyRouter = require("./party")
-router.use("/", partyRouter)
+const partyRouter = require("./party");
+router.use("/", partyRouter);
 
 // users
-const usersRouter = require("./user")
-router.use("/", usersRouter)
+const usersRouter = require("./user");
+router.use("/", usersRouter);
 
-module.exports = router
+module.exports = router;
