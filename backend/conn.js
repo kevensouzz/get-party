4a05d3693adf1162155b12f474e56516c1e2572b
Keven Souza <kevenssouza1@gmail.com>
2023-08-11 18:09:42 -0300

    update conn to db

--- backend/conn.js
+++ backend/conn.js
@@ -6,19 +6,12 @@ const kevdbUSER = process.env.DB_USER_KEV;
 const kevdbPASSWORD = process.env.DB_PASSWORD_KEV;
 const kevdbURL = `mongodb+srv://${kevdbUSER}:${kevdbPASSWORD}@${kevdbHOST}`;
 
-// const leodbHOST = process.env.DB_HOST_LEO;
-// const leodbUSER = process.env.DB_USER_LEO;
-// const leodbPASSWORD = process.env.DB_PASSWORD_LEO;
-// const leodbURL = `mongodb+srv://${leodbUSER}:${leodbPASSWORD}@${leodbHOST}/mern-app?retryWrites=true&w=majority`;
-
 async function main() {
   try {
     mongoose.set("strictQuery", true);
-
-    // await mongoose.connect(leodbURL)
-
     await mongoose.connect(kevdbURL);
     console.log("CONNECTED TO DB!");
+    
   } catch (e) {
     console.error(e);
   }
