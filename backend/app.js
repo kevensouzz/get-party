1726060e7969478c9cd3f8e9ea901f1e6913600b
Keven Souza <kevenssouza1@gmail.com>
2023-08-12 18:20:52 -0300

    delete routes directory

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const partyController = require("./controllers/partyController");
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-// solve cors
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
-// db connection
 const db = require("./db");
 
-// routes
-const routes = require("./routes/router");
-app.use("/", routes);
+app.post("/", (req, res) => partyController.create(req, res));
+app.get("/", (req, res) => partyController.getAll(req, res));
+app.get("/:id", (req, res) => partyController.get(req, res));
+app.delete("/:id", (req, res) => partyController.delete(req, res));
+app.put("/:id", (req, res) => partyController.update(req, res));
 
 db()
   .then(() => {
--- backend/routes/router.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-const partyRouter = require("./party");
-router.use("/", partyRouter);
-
-module.exports = router;
